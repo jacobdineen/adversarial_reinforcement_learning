c7375c830642fca3f938293c7ea640ab46b34635
jake <jdineen@asu.edu>
2023-10-23 10:44:39 -0700

    change loss func

--- src/train.py
+++ src/train.py
@@ -73,18 +73,23 @@ def policy(state, epsilon, q_net, env) -> int:
     return action_index
 
 
-def train(env: ImagePerturbEnv, q_net: QNetwork, optimizer: optim.Optimizer, num_episodes: int = 100) -> QNetwork:
+def train(
+    env: ImagePerturbEnv,
+    q_net: QNetwork,
+    optimizer: optim.Optimizer,
+    num_episodes: int = 100,
+    epsilon: float = 0.1,
+    gamma: float = 0.95,
+    update_freq: int = 1,
+    batch_size: int = 256,
+    decay: float = 0.99,
+) -> QNetwork:
     """
     Creates an environment from the dataloader and model and trains a Q-Network - returns the trained Q-Network.
     """
 
     # Initialize Replay Buffer and Other Training Parameters
     buffer: deque[tuple[torch.Tensor, int, float, torch.Tensor, bool]] = deque(maxlen=10000)
-    batch_size = 256  # sample 64 experiences from the replay buffer every time
-    gamma = 0.5  # discount factor
-    epsilon = 0.1  # start with 10% exploration
-    update_freq = 1  # update epsilon every 100 episodes
-
     episode_rewards = []
 
     for episode in tqdm(range(num_episodes)):
@@ -117,8 +122,9 @@ def train(env: ImagePerturbEnv, q_net: QNetwork, optimizer: optim.Optimizer, num
                 curr_Q = q_net(states).gather(1, actions.unsqueeze(-1)).squeeze(-1)
                 next_Q = q_net(next_states).max(1)[0]
                 target_Q = rewards + (gamma * next_Q) * (1 - dones)
-
-                loss = nn.functional.mse_loss(curr_Q, target_Q.detach())
+                # huber loss
+                # less sensitive to outliers
+                loss = nn.functional.smooth_l1_loss(curr_Q, target_Q.detach())
                 optimizer.zero_grad()
                 loss.backward()
                 optimizer.step()
@@ -127,33 +133,47 @@ def train(env: ImagePerturbEnv, q_net: QNetwork, optimizer: optim.Optimizer, num
         episode_rewards.append(episode_reward)
 
         if episode % update_freq == 0:
-            epsilon *= 0.99
+            epsilon *= decay
 
     logging.info(f"Completed Training")
     return q_net, episode_rewards
 
 
 if __name__ == "__main__":
+    num_episodes = 100  # number of episodes to train for
+    learning_rate = 10e-2  # learning rate for optimizer
+    attack_budget = 100  # max number of perturbations
+    reward_lambda = 1
+    batch_size = 256  # sample 64 experiences from the replay buffer every time
+    gamma = 0.95  # discount factor
+    epsilon = 0.5  # start with 50% exploration
+    update_freq = 1  # update epsilon every 100 episodes
+    decay = 0.99  # decay rate for epsilon
+
     # cifar10 dataloader
     dataloader = get_cifar_dataloader()
     # classififer
     model = load_model()
     # env
-    env = ImagePerturbEnv(
-        dataloader=dataloader, model=model, attack_budget=50, lambda_=1
-    )  # lambda_ used in reward to penalize more changes
+    env = ImagePerturbEnv(dataloader=dataloader, model=model, attack_budget=attack_budget, lambda_=reward_lambda)
 
     # Initialize Q-Network and Optimizer here, and pass them to train
     # input_shape = env.observation_space.shape
     n_actions = env.action_space.n
     q_net = QNetwork(n_actions).to(DEVICE)
-
-    learning_rate = 10e-3
     optimizer = optim.Adam(q_net.parameters(), lr=learning_rate)
 
-    num_episodes = 5000
     logging.info(f"Starting Training with num_episodes = {num_episodes}")
-    trained_qnet, episode_rewards = train(env, q_net, optimizer, num_episodes)
+    trained_qnet, episode_rewards = train(
+        env,
+        q_net,
+        optimizer,
+        num_episodes,
+        epsilon,
+        gamma,
+        update_freq,
+        batch_size,
+    )
     print(episode_rewards)
 
     # Plot the episode rewards
