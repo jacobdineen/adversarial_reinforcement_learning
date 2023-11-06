f6e96bf6c3b0fcfebd95b431430e413855c88e8e
jake <jdineen@asu.edu>
2023-11-06 12:37:32 -0700

    enable ability to track more than 100 episodes rewards

--- src/train.py
+++ src/train.py
@@ -5,6 +5,7 @@ import logging
 import pandas as pd
 import torch
 from stable_baselines3 import PPO
+from stable_baselines3.common.callbacks import BaseCallback
 
 from src.env import ImagePerturbEnv
 from src.plotting import plot_rewards_and_cumulative
@@ -15,11 +16,38 @@ logging.basicConfig(level=logging.INFO)
 DEVICE = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 SEED = 42
 
+
+class RewardLoggerCallback(BaseCallback):
+    def __init__(self, check_freq):
+        super().__init__()
+        self.check_freq = check_freq
+        self.all_rewards = []
+        self.all_lengths = []
+        self.all_times = []
+
+    def _on_step(self) -> bool:
+        # Check if it's time to log episode information
+        if self.n_calls % self.check_freq == 0:
+            # Retrieve the episode information from the buffer
+            ep_info = self.model.ep_info_buffer
+            if ep_info:
+                # Extract the latest episode information
+                info = ep_info[-1]
+                self.all_rewards.append(info["r"])
+                self.all_lengths.append(info["l"])
+                self.all_times.append(info["t"])
+        return True
+
+    def get_episode_info(self):
+        """Retrieve the episode information."""
+        return {"rewards": self.all_rewards, "lengths": self.all_lengths, "times": self.all_times}
+
+
 parser = argparse.ArgumentParser(description="Train an agent to perturb images.")
 parser.add_argument("--dataset_name", type=str, default="cifar", help="dataset to use. mnist of cifar")
 
-parser.add_argument("--num_episodes", type=int, default=100, help="Number of episodes to run.")
-parser.add_argument("--batch_size", type=int, default=256, help="Batch size for training.")
+parser.add_argument("--num_episodes", type=int, default=1000, help="Number of episodes to run.")
+parser.add_argument("--batch_size", type=int, default=512, help="Batch size for training.")
 parser.add_argument("--val_split", type=float, default=0.2, help="Holdout data for validation and testing.")
 parser.add_argument(
     "--train_limit", type=int, default=None, help="Training dataloader limit - useful for debugging shorter runs."
@@ -73,25 +101,20 @@ if __name__ == "__main__":
     # )
 
     # Training here
+    # callback necessary because defaults to last 100 episodes
+    callback = RewardLoggerCallback(check_freq=steps_per_episode)
     model = PPO("MlpPolicy", train_env, device=DEVICE, verbose=1, n_steps=steps_per_episode, batch_size=batch_size)
     logging.info(f"model device: {model.device}")
-    model.learn(
-        total_timesteps=total_timesteps,
-        progress_bar=prog_bar,
-    )
+    model.learn(total_timesteps=total_timesteps, progress_bar=prog_bar, callback=callback)
     logging.info("Training complete.")
     logging.info(f"Saving model to {model_save_path}")
     model.save(model_save_path)
 
-    ep_info_buffer = model.ep_info_buffer
-    print(ep_info_buffer)
-    rewards = [info["r"] for info in ep_info_buffer]
-    lengths = [info["l"] for info in ep_info_buffer]
-    times = [info["t"] for info in ep_info_buffer]
-
-    df = pd.DataFrame(ep_info_buffer)
-    df.rename(columns={"r": "rewards", "l": "lengths", "t": "times"}, inplace=True)
+    episode_info = callback.get_episode_info()
+    df = pd.DataFrame(episode_info)
+    df.rename(columns={"rewards": "rewards", "lengths": "lengths", "times": "times"}, inplace=True)
     df.to_csv(f"{model_performance_save_path}.csv")
     logging.info(f"Dataframe saved to {model_performance_save_path}")
 
-    plot_rewards_and_cumulative(rewards)
+    # If `plot_rewards_and_cumulative` requires just the rewards, you can extract them
+    plot_rewards_and_cumulative(df["rewards"])
